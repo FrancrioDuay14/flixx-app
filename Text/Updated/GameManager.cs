9434c3c44d691842f3595a5713b25f24124db083
FrancrioDuay14 <francduay@gmail.com>
2023-09-13 16:42:26 +0800

    updated

---
+++ Text/Updated/GameManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+  public Text scoreText;
+    private int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateScoreText();
+    }
+
+    // Scoring , Score Text
+    public void AddScore(int amount){  // Kung ilang points kapag na destroy mo yung isang coin
+        score += amount;
+        UpdateScoreText();
+    }
+
+    void UpdateScoreText(){
+        scoreText.text = "Score: " + score;
+
+    }
+}
